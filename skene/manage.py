121e639b16f67964d3527f0ec413b11282534e90
Kauhsa <kauhsa@viuhka.fi>
2012-01-17 18:25:37 +0200

    Changed imports.

--- skene/manage.py
+++ skene/manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from django.core.management import execute_manager
 import imp
-from skene import settings
+import settings
 
 try:
     imp.find_module('settings') # Assumed to be in the same directory.

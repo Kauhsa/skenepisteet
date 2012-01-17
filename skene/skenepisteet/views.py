19fe5e7eb0f488f34d64656f73ed213fb8e1ef0c
Kauhsa <kauhsa@viuhka.fi>
2012-01-17 21:48:38 +0200

    Remove import no longer in use.

--- skene/skenepisteet/views.py
+++ skene/skenepisteet/views.py
@@ -5,7 +5,7 @@ from django.db.models import Sum
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from django.utils import simplejson
-from skene.skenepisteet.forms import SuggestionForm, LoginForm
+from skene.skenepisteet.forms import SuggestionForm
 from skene.skenepisteet.models import Scener, ScenePointEvent
 from djpjax import pjax
 from django.contrib import messages

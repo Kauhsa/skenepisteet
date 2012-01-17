b5c51af362cf70eac67a795631bfcea079987592
Kauhsa <kauhsa@viuhka.fi>
2012-01-17 21:39:13 +0200

    Show and do sum only accepted ScenePointEvents.

--- skene/skenepisteet/views.py
+++ skene/skenepisteet/views.py
@@ -17,13 +17,14 @@ def index(request):
     # sorting manually (not with order_by) because it fails if there is no events on scener
     # as SQL backend doesn't treat NULL as 0 when sorting - we also need to manually change
     # any None in points to 0 to manual sorting to work
-    sceners = list(Scener.objects.annotate(points=Sum('scenepointevent__points')))
+    sceners = Scener.objects.filter(scenepointevent__accepted=True).annotate(points=Sum('scenepointevent__points'))
+    sceners = list(sceners)
     for scener in sceners:
         if not scener.points:
             scener.points = 0
     sceners = sorted(sceners, key=attrgetter('points'), reverse=True)
 
-    activity = ScenePointEvent.objects.order_by('-award_date')[:5]
+    activity = ScenePointEvent.objects.filter(accepted=True).order_by('-award_date')[:5]
     return TemplateResponse(request, "index.html", {"sceners": sceners, "activities": activity})
 
 def info_popup(request, scener_id=None):

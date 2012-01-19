fc4e71a7353ebae54dc04648a26d0cef2d411071
Kauhsa <kauhsa@viuhka.fi>
2012-01-19 21:36:44 +0200

    Admin panel updated.

--- skene/skenepisteet/admin.py
+++ skene/skenepisteet/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from skene.skenepisteet.models import Scener, ScenePointEvent
 
+class ScenePointEventAdmin(admin.ModelAdmin):
+    list_display = ('scener', 'points', 'description', 'accepted')
+    list_filter = ('accepted',)
+
 admin.site.register(Scener)
-admin.site.register(ScenePointEvent)
+admin.site.register(ScenePointEvent, ScenePointEventAdmin)

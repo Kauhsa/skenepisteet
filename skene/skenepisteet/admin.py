d8adf68b2a8cae9f96b41fa16ae2f1f327e6dc36
Kauhsa <kauhsa@viuhka.fi>
2012-01-20 09:43:02 +0200

    Added inline admin for scene point events.

--- skene/skenepisteet/admin.py
+++ skene/skenepisteet/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from skene.skenepisteet.models import Scener, ScenePointEvent
 
+class InlineScenePointEvent(admin.TabularInline):
+    model = ScenePointEvent
+    extra = 0
+
+class ScenerAdmin(admin.ModelAdmin):
+    inlines = (InlineScenePointEvent,)
+
 class ScenePointEventAdmin(admin.ModelAdmin):
     list_display = ('scener', 'points', 'description', 'accepted', 'award_date')
     list_filter = ('accepted',)
 
-admin.site.register(Scener)
+admin.site.register(Scener, ScenerAdmin)
 admin.site.register(ScenePointEvent, ScenePointEventAdmin)

9068be00809c675d3c969677ab90be9e4d09eda2
Kauhsa <kauhsa@viuhka.fi>
2012-01-23 22:37:43 +0200

    Move DEBUG setting to environment variable.

--- skene/settings.py
+++ skene/settings.py
@@ -2,7 +2,7 @@
 from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
 import os
 
-DEBUG = False
+DEBUG = os.environ.get('DJANGO-DEBUG', 'false') == 'true'
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (

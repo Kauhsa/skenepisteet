ace4dd59fcbc08370aa53dca141079c9565cb157
Kauhsa <kauhsa@viuhka.fi>
2012-01-23 22:55:53 +0200

    Add new action to admin page.

--- skene/skenepisteet/admin.py
+++ skene/skenepisteet/admin.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 from django.contrib import admin
 from skene.skenepisteet.models import Scener, ScenePointEvent
 
@@ -11,6 +12,11 @@ class ScenerAdmin(admin.ModelAdmin):
 class ScenePointEventAdmin(admin.ModelAdmin):
     list_display = ('scener', 'points', 'description', 'accepted', 'award_date')
     list_filter = ('accepted',)
+    actions = ('set_accepted',)
+
+    def set_accepted(self, request, queryset):
+        queryset.update(accepted=True)
+    set_accepted.short_description = u"Merkitse hyväksytyiksi"
 
 admin.site.register(Scener, ScenerAdmin)
 admin.site.register(ScenePointEvent, ScenePointEventAdmin)
